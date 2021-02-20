e3568c9a2856d0e45c18bb6e43b81f8a4c63024e
Glorfin <wvunder@gmail.com>
2021-02-20 23:44:06 +0300

    CreateServer() and StartListening() functions

--- src/SOCK.cpp
+++ src/SOCK.cpp
@@ -1,18 +1,104 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
 #include "headers/SOCK.h"
 
-int Server::CreateServer()
+#define DEFAULT_BUFLEN 512
+
+int Server::CreateServer(PCSTR port)
 {
-	// socket baðlantýsý oluþtur ve geri döndür
-	return 1;
+    WSADATA wsaData;
+    int iResult;
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo* result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 0;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    iResult = getaddrinfo(NULL, port, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 0;
+    }
+
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 0;
+    }
+
+    Server::StartListening(ListenSocket, result);
+	return ListenSocket;
 }
 
-int Server::ReadData(int sock)
+int Server::StartListening(int sock, addrinfo* result)
 {
-	return 1;
+    int iResult = bind(sock, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(sock);
+        WSACleanup();
+        return 0;
+    }
+
+    freeaddrinfo(result);
+
+    iResult = listen(sock, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(sock);
+        WSACleanup();
+        return 0;
+    }
+
+    SOCKET ClientSocket = accept(sock, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        printf("accept failed with error: %d\n", WSAGetLastError());
+        closesocket(sock);
+        WSACleanup();
+        return 0;
+    }
+
+    return 1;
+}
+
+int Server::OnDataRecieve(int sock, int recvlen, char dRecieved)
+{
+
+    return 1;
 }
 
 int Server::CloseServer(int sock)
 {
-	// socket baðlantýsýný kapat
+    closesocket(sock);
+    WSACleanup();
 	return 1;
 }
--- src/headers/SOCK.h
+++ src/headers/SOCK.h
@@ -1,11 +1,26 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
 class Server
 {
 public:
+	// Variables
 	int connection = 0;
-
-	int CreateServer();
-	int ReadData(int sock);
+	
+	// Functions
+	int CreateServer(PCSTR port);
+	int StartListening(int sock, addrinfo* result);
 	int CloseServer(int sock);
+
+	// Callbacks
+	int OnDataRecieve(int sock, int recvlen, char dRecieved);
 };
